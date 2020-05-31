3f49378e26ad2fa3a68d91f62b9f60c02a1cbdfc
Michael_Harris <mwharris7@gmail.com>
2020-05-30 17:32:13 -0700

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,131 @@
+class TicTacToe
+    
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"      
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"   
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+    def input_to_index(user_input)
+        user_input.to_i - 1
+    end
+
+    def move(index, current_player = "X")
+        @board[index] = current_player
+    end
+
+    def position_taken?(index)
+        !(@board[index].nil? || @board[index] == " ")
+    end
+
+    def valid_move?(index)
+        index.between?(0, 8) && !position_taken?(index)
+    end
+
+    def turn_count
+        counter = 0
+        @board.each do |token|
+            if token == "X" || token == "O"
+                counter += 1
+            end
+        end
+        return counter
+    end
+    
+    def current_player
+        if turn_count.even?
+            return "X"
+        else
+            return "O" 
+        end
+    end
+    
+    def won?
+        WIN_COMBINATIONS.each {|win_combo|
+            index_0 = win_combo[0]
+            index_1 = win_combo[1]
+            index_2 = win_combo[2]
+            
+            position_1 = @board[index_0]
+            position_2 = @board[index_1]
+            position_3 = @board[index_2]
+
+            if position_1 == "X" && position_2 == "X" && position_3 == "X"
+                return win_combo
+            elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+                return win_combo
+            end
+        }
+        return false
+    end
+    
+    def full?
+        @board.all? {|index| index == "X" || index == "O"}
+    end
+    
+    def draw?
+        (!won? && full?)
+    end
+    
+    def over?
+        (won? || draw?)
+    end
+    
+    def winner
+        index = []
+        index = won?
+        if index == false
+            return nil
+        else
+            if @board[index[0]] == "X"
+                return "X"
+            else
+                return "O"
+            end
+        end
+    end
+
+    def turn
+        puts "Please enter 1-9:"               # ask for input
+        user_input = gets.chomp
+        index = input_to_index(user_input)     # translate input into index
+        if valid_move?(index)
+            player_token = current_player                  # if index is valid
+            move(index, player_token)          # make the move for index
+            display_board                      # show the board
+        else
+            turn
+        end
+    end
+    
+    def play
+        puts "Welcome to Tic Tac Toe!"
+        display_board
+        until over?
+            turn
+        end
+        if won?
+            puts "Congratulations #{winner}!"
+        elsif draw?
+            puts "Cat's Game!"
+        end
+    end
+
+end
